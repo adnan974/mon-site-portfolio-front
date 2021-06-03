2cc5dee62bc4b9056531bf18c133b11d760ae640
Adnan <adnan.goulamaly@gmail.com>
2021-06-03 18:54:17 +0400

    feature: creation d'une navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './module/home/pages/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar'; 
-
+import {MatToolbarModule,} from '@angular/material/toolbar'; 
+import {MatButtonModule} from '@angular/material/button'; 
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -15,11 +17,14 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     
   ],
   imports: [
-    
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatButtonModule
   
   ],
   providers: [],
